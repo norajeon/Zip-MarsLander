fa4969350e7cfd564099f3c327447dc75b7e4816
nora <sxj042@gmail.com>
2023-04-02 14:23:52 -0400

    a

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,7 +3,16 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
-        
+
+        if (burn < 100) {
+            burn = (status.Velocity * status.Velocity) / (2 * status.Altitude) + 99;
+        } else {
+            burn = (status.Velocity * status.Velocity) / (2 * status.Altitude);
+        }
+
+
+
+
         System.out.println(burn); /*hack!*/
         return burn;
     }
